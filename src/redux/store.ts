234661bd39689456da1d1f87773aeaaeb6c4d788
Roman <Roman.Mettler@gmail.com>
2020-12-12 12:56:42 -0300

    Añade reduxDevTools como dependencia

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,7 @@
 import { createStore } from 'redux'
 import reducerTest from './reducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export const store = createStore(reducerTest)
+export const store = createStore(reducerTest,
+    composeWithDevTools()
+)
