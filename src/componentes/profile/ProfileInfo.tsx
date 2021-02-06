b71041ad2238c87ab6ddcb90b0daf5124c742dd6
Roman <Roman.Mettler@gmail.com>
2021-02-06 13:40:49 -0300

    muestra numero de gente seguida en perfil

--- src/componentes/profile/ProfileInfo.tsx
+++ src/componentes/profile/ProfileInfo.tsx
@@ -1,5 +1,6 @@
 import { Box, createStyles, makeStyles, Theme, Typography } from '@material-ui/core'
 import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
+import GroupIcon from '@material-ui/icons/Group';
 import React from 'react'
 import { User } from '../../types/User'
 import moment from 'moment'
@@ -23,16 +24,23 @@ interface Props {
 
 export const ProfileInfo: React.FC<Props> = ({ user }) => {
     const classes = useStyles()
-    const { name, surname, createdAt, bio } = user
+    const { name, surname, createdAt, bio, followersNumb } = user
     const joinedDate = moment(createdAt).format("MMM Do YY");
 
     return (
         <Box display="flex" flexDirection="column" className={classes.container}>
             <Typography variant="h5" className={classes.header}>{name} {surname}</Typography>
-            <Typography variant="body1">{bio || "bio vacio ekisde"}</Typography>
+            <Typography variant="body1">{bio || "i haven't set my bio yet!"}</Typography>
             <Box display="flex">
-                <CalendarTodayOutlinedIcon></CalendarTodayOutlinedIcon>
-                <Typography>Joined at {joinedDate}</Typography>
+                <Box display="flex">
+                    <CalendarTodayOutlinedIcon></CalendarTodayOutlinedIcon>
+                    <Typography>Joined at {joinedDate}</Typography>
+                </Box>
+                
+                {followersNumb && <Box display="flex">
+                    <GroupIcon></GroupIcon>
+                    <Typography>Following {followersNumb} {followersNumb === 1 ? "person" : "people"}</Typography>
+                </Box>}
             </Box>
         </Box>
     )
