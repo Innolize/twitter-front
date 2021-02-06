cd9543d6ec4d7dd4f2efa50140112cf1d2d8f09f
Roman <Roman.Mettler@gmail.com>
2021-02-06 13:20:27 -0300

    permite agregar emojis a comentarios

--- src/componentes/main/comment/CreateComment.tsx
+++ src/componentes/main/comment/CreateComment.tsx
@@ -1,10 +1,14 @@
 import { Box, Button, createStyles, makeStyles, TextField, Theme } from '@material-ui/core'
+import { IEmojiData } from 'emoji-picker-react'
 import React, { useState } from 'react'
 import { createComment } from '../../../api/comment/createComment'
+import { EmojiButton } from '../../common/EmojiButton'
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
+            display: "flex",
+            alignItems: "center",
             padding: "10px",
             margin: "0px 20px",
             border: "1px solid rgba(0, 0, 0, 0.12)"
@@ -35,9 +39,14 @@ export const CreateComment: React.FC<Props> = ({ postId }) => {
         console.log(response)
     }
 
+    const addEmoji = (e: React.MouseEvent<Element, MouseEvent>, data: IEmojiData) => {
+        setText(c => c + data.emoji)
+    }
+
     return (
         <Box display="flex" className={classes.root}>
             <TextField value={text} onChange={textfieldOnChange} variant="outlined" placeholder='Write something...' className={classes.textArea} ></TextField>
+            <EmojiButton onEmojiClick={addEmoji}></EmojiButton>
             <Button onClick={buttonOnClick}> Send</Button>
         </Box>
     )
